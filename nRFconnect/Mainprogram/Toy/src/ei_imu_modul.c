497d3a90ffff2ae84398b8139324ecf6b264eac1
HubicekHubik <hubertottcz@gmail.com>
2026-04-10 12:21:20 +0200

    changed sampling freq saperated gest evlaluation form call back

--- nRFconnect/Mainprogram/Toy/src/ei_imu_modul.c
+++ nRFconnect/Mainprogram/Toy/src/ei_imu_modul.c
@@ -1,6 +1,7 @@
 #include "ei_imu_modul.h"
 #include "app_state.h"
 #include "toy_utils.h"
+#include "math.h"
 //=======================================//
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
@@ -19,15 +20,15 @@ LOG_MODULE_REGISTER(EI_IMU, CONFIG_LOG_DEFAULT_LEVEL);
 #include <zephyr/sys/util.h>
 
 #include <ei_wrapper.h>
-#define SAMPLING_FREQ 104
+#define SAMPLING_FREQ 52
 #define RESULTS_PRINT 1
 bool ei_canceled = false;
 #define HISTORY_LEN 14
 #define NUM_CLASSES 5
 #define MIN_DETECTION_COUNT 6
 #define STRAIGHT_IDX 5
-#define DEBUG_RESULT_CB // coment this to use the old function of result_ready
-#define IDLEFOLLOWUP_TREASHOLD 18
+//#define DEBUG_RESULT_CB  coment this to use non debug result_ready
+#define IDLEFOLLOWUP_TREASHOLD 60
 
 uint8_t hystor_index;
 float label_history[HISTORY_LEN][NUM_CLASSES];
@@ -36,14 +37,14 @@ uint8_t idlefollowup = 0;
 
 static void result_ready_cb(int err);
 
-void resolve_classification(float values[5], float anomaly);
+void resolve_classification(float values[8], float anomaly);
 
 #define CLASS_THREAD_STACK_SIZE 2024
 #define CLASS_THREAD_PRIORITY 6
 K_THREAD_STACK_DEFINE(class_stack_area, CLASS_THREAD_STACK_SIZE);
 static struct k_thread class_thread_data;
 
-K_MSGQ_DEFINE(class_msgq, sizeof(struct toy_events), 16, 4);
+K_MSGQ_DEFINE(class_msgq, sizeof(struct ei_ev), 16, 4);
 
 void class_processing_thread(void *p1, void *p2, void *p3);
 
@@ -80,15 +81,12 @@ void lsm6dsl_sleep() {
 	int err;
     const struct device *lsm6dsl_dev = DEVICE_DT_GET_ONE(st_lsm6dsl);
 
-    // 1) Zastavit trigger
     sensor_trigger_set(lsm6dsl_dev, NULL, NULL);
 
-    // 2) Vypnout ODR → IMU přestane posílat data
     struct sensor_value off_odr = {0, 0};
     sensor_attr_set(lsm6dsl_dev, SENSOR_CHAN_ACCEL_XYZ,
                     SENSOR_ATTR_SAMPLING_FREQUENCY, &off_odr);
 
-    // 3) Vyčistit queue
     k_msgq_purge(&imu_msgq);
 	bool canceld;
 	err = ei_wrapper_clear_data(&canceld);
@@ -111,7 +109,7 @@ void lsm6dsl_ei_wake() {
 		LOG_INF("- %s", ei_wrapper_get_classifier_label(i));
 	}
 	*/
-	err = ei_wrapper_start_prediction(1, 0);
+	err = ei_wrapper_start_prediction(0, 13);
 	if (err) {
 		LOG_ERR("Cannot start prediction (err: %d)", err);
 	}
@@ -190,14 +188,14 @@ static void imu_ini_data_forw(void *arg1, void *arg2, void *arg3) {
 	lsm6dsl_ei_wake();
 
 	float f_val_buffer[6];
+	float Am_Gm[2];
 	struct sensor_value acc_gyro_buffer[6];
 	while (true) {
 		//k_sem_take(&run_sem, K_FOREVER); // pokud je OFF, vlákno se zastaví
 		//k_sem_give(&run_sem);
 
 		k_msgq_get(&imu_msgq, acc_gyro_buffer, K_FOREVER);
-		if (k_msgq_num_used_get(&imu_msgq) > (200 * 3 / 4))
-		{
+		if (k_msgq_num_used_get(&imu_msgq) > (200 * 3 / 4)) {
 			LOG_WRN("Audio queue > 75%% full!\n");
 		}
 		f_val_buffer[0] = sensor_value_to_float(&acc_gyro_buffer[0]);
@@ -208,10 +206,12 @@ static void imu_ini_data_forw(void *arg1, void *arg2, void *arg3) {
 		f_val_buffer[5] = sensor_value_to_float(&acc_gyro_buffer[5]);
 		//LOG_INF("This are the walues form sensor Ax:%f Ay:%f Az:%f Gx:%f Gy:%f  Gz:%f",f_val_buffer[0], f_val_buffer[1], f_val_buffer[2], f_val_buffer[3], f_val_buffer[4], f_val_buffer[5]);
 		if (!audioPlaying) {
-			err = ei_wrapper_add_data(f_val_buffer, 6);
+			Am_Gm[0] = sqrt( f_val_buffer[0]*f_val_buffer[0] + f_val_buffer[1]*f_val_buffer[1] + f_val_buffer[2]*f_val_buffer[2]);
+			Am_Gm[1] = sqrt( f_val_buffer[3]*f_val_buffer[3] + f_val_buffer[4]*f_val_buffer[4] + f_val_buffer[5]*f_val_buffer[5]);
+			err = ei_wrapper_add_data(Am_Gm, 2);
 			if (err) {
-				LOG_INF("Cannot provide input data (err: %d)\n", err);
-				LOG_ERR("Increase CONFIG_EI_WRAPPER_DATA_BUF_SIZE\n");
+				//LOG_INF("Cannot provide input data (err: %d)\n", err);
+				//LOG_ERR("Increase CONFIG_EI_WRAPPER_DATA_BUF_SIZE\n");
 			}
 		} else {
 		}
@@ -220,13 +220,17 @@ static void imu_ini_data_forw(void *arg1, void *arg2, void *arg3) {
 
 #ifdef DEBUG_RESULT_CB
 static void result_ready_cb(int err) {
+	LOG_INF("Resutl ready err: %d",err);
+}
+#else
+static void result_ready_cb(int err) {
 	if (err) {
 		LOG_INF("Result ready callback returned error (err: %d)\n", err);
 		return;
 	}
 
 	struct ei_ev tx_class = {
-		.anomaly = 404.0f,
+		.anomaly = 0.0f,
 		.values = {0}
 	};
 	const char *label;
@@ -270,7 +274,7 @@ static void result_ready_cb(int err) {
 
 	k_msgq_put(&class_msgq, &tx_class, K_NO_WAIT);
 
-	err = ei_wrapper_start_prediction(1, 0);
+	err = ei_wrapper_start_prediction(0, 13);
 
 	if (RESULTS_PRINT) {
 		if (err) {
@@ -280,96 +284,6 @@ static void result_ready_cb(int err) {
 			LOG_INF("Prediction restarted...");
 		}
 	}
-}
-#else
-static void result_ready_cb(int err)
-{
-	if (err)
-	{
-		printk("Result error: %d\n", err);
-		return;
-	}
-
-	/* 1. Vynuluj řádek, který právě přepisujeme
-	   a odečti jeho příspěvek ze součtového pole */
-	for (int c = 0; c < NUM_CLASSES; c++)
-	{
-		labels_value[c] -= label_history[hystor_index][c];
-		label_history[hystor_index][c] = 0.0f;
-	}
-
-	/* 2. Získej všechny výsledky pro aktuální okno */
-	const char *dummy_label; /* řetězec ignorujeme */
-	float prob;
-	size_t lbl_idx; /* ← tady dostaneme číselný index!   */
-
-	while (!ei_wrapper_get_next_classification_result(&dummy_label, &prob, &lbl_idx))
-	{
-		if (lbl_idx < NUM_CLASSES)
-		{
-			label_history[hystor_index][lbl_idx] = prob;
-			labels_value[lbl_idx] += prob; /* inkrementální součet */
-		}
-	}
-
-	/* 3. Posuň index v kruhovém bufferu */
-	hystor_index = (hystor_index + 1) % HISTORY_LEN;
-
-	/* 4. Najdi třídu s největším součtem pravděpodobností */
-	int max_i = 0;
-	float max_v = labels_value[0];
-	for (int c = 1; c < NUM_CLASSES; c++)
-	{
-		if (labels_value[c] > max_v)
-		{
-			max_v = labels_value[c];
-			max_i = c;
-		}
-	}
-
-	/* 5. Přepočítej na „počty hlasů“ (≈ jestli průměr > 0.5) */
-	int votes = 0;
-	for (int k = 0; k < HISTORY_LEN; k++)
-	{
-		if (label_history[k][max_i] > 0.55f)
-		{ /* práh pro „hlas“ */
-			votes++;
-		}
-	}
-
-	if (max_i == 0)
-	{
-		++idlefollowup;
-		// printk("Increasing folllowup to %d\n", idlefollowup);
-	}
-	else
-	{
-		idlefollowup = 0;
-	}
-
-	/* 6. Vyhlášení gesta – ignorujeme 'straight' */
-	if (votes >= MIN_DETECTION_COUNT && max_i != STRAIGHT_IDX && max_i != 3 && ((max_i != 0) || (idlefollowup == 230)))
-	{
-		struct audio_ev g = {
-			.cmd = PKT_GEST,
-			.gesture_id = max_i, /* posíláme číslo labelu */
-			.pipeprefix = DEVICE_PREFIX};
-
-		if (!audioPlaying) // maby there is a need of tracking state of audiostream
-		{
-			printk("Gesture detected: %s\n", ei_wrapper_get_classifier_label(max_i));
-			k_msgq_put(&aud_event_msgq, &g, K_NO_WAIT);
-		}
-
-		/* vyčisti buffer, ať se gesto nehlásí znovu hned za chvíli */
-		memset(label_history, 0, sizeof(label_history));
-		memset(labels_value, 0, sizeof(labels_value));
-		hystor_index = 0;
-		idlefollowup = 0;
-	}
-
-	/* 7. Spusť další sliding-window inferenci */
-	ei_wrapper_start_prediction(0, 9);
 }
 #endif
 
@@ -386,11 +300,15 @@ void class_processing_thread(void *p1, void *p2, void *p3) {
 	
 }
 
-void resolve_classification(float values[5], float anomaly) {
-	char buf[200];
+void resolve_classification(float values[8], float anomaly) {
+	char buf[240];
 	int len = 0;
 	int max_res = ei_wrapper_get_classifier_label_count();
 	int err;
+	static uint8_t circle_count = 0;
+	static uint8_t roll_count = 0;
+	static uint8_t tap_count = 0;
+
 	for (size_t i = 0; i < max_res; i++) {
 		if(master_conn != NULL) {
 		len += snprintf(buf + len, sizeof(buf) - len, 
@@ -398,8 +316,43 @@ void resolve_classification(float values[5], float anomaly) {
                             (double)values[i], ei_wrapper_get_classifier_label(i));
 		}
 
-		if (( i != 4) && (values[i] > 0.95f) &&
-			((i != 2) || (idlefollowup >= IDLEFOLLOWUP_TREASHOLD))) {
+		if (i == 2) {
+			if (values[i] >= 0.99f) {
+				idlefollowup += 1;
+				//printk("idlefollowup increased to %d\n", idlefollowup);
+			} else {
+				idlefollowup = 0;
+				//printk("idlefollowup voided se to 0\n");
+			}
+		}
+
+		if (i == 0) {
+			if(values[i] > 0.95f){
+				circle_count +=1;
+			} else {
+				circle_count = 0;
+			}
+		}
+
+		if (i == 4) {
+			if(values[i] > 0.95f){
+				roll_count +=1;
+			} else {
+				roll_count = 0;
+			}
+		}
+		if (i == 6) {
+			if(values[i] > 0.95f){
+				tap_count +=1;
+			} else {
+				tap_count = 0;
+			}
+		}
+		if (( i != 3) && (values[i] > 0.95f) && (anomaly < 0.4f) &&
+			((i != 2) || (idlefollowup >= IDLEFOLLOWUP_TREASHOLD)) &&
+			((i != 0) || (circle_count == 2)) &&
+			((i != 4) ||(roll_count == 1)) &&
+			((i != 6) ||(tap_count == 2))) {
 			struct toy_events tx_gest = {
 				.type = MT_GEST,
 				.payload.gest.type = i,
@@ -407,35 +360,31 @@ void resolve_classification(float values[5], float anomaly) {
 				.payload.gest.anomaly = (uint16_t)(anomaly *anomaly * 100.0f),
 				.payload.gest.sign = 0
 			};
+			if (i == 7) {
+				tx_gest.payload.gest.type = 3;
+			}
 			if (anomaly < 0) {
 				tx_gest.payload.gest.sign = 1;
 			}
 			if (!audioPlaying) {
 				LOG_INF("Gesture detected: %s index is: %d prob: %2f anomaly: %f", ei_wrapper_get_classifier_label(i), i, (double)values[i], (double)anomaly);
 				k_msgq_put(&sysfb_msgq, &tx_gest, K_NO_WAIT);
+				bool canceled;
+				ei_wrapper_clear_data(&canceled);
+				ei_wrapper_start_prediction(0, 1);
 			}
 			idlefollowup = 0;
-		}
-
-		if (i == 2) {
-			if (values[i] >= 0.99f)
-			{
-				idlefollowup += 1;
-				//printk("idlefollowup increased to %d\n", idlefollowup);
-			}
-			else
-			{
-				idlefollowup = 0;
-				//printk("idlefollowup voided se to 0\n");
-			}
+			circle_count = 0;
+			roll_count = 0;
+			tap_count = 0;
 		}
 	}
 
-	/*if(master_conn != NULL) {
+	if(master_conn != NULL) {
 		len += snprintf(buf + len, sizeof(buf) - len, "Anomaly: %.2f", (double)anomaly);
-        uint8_t packet[210];
+        uint8_t packet[240];
         packet[0] = MT_DEBUG_DATA;
         memcpy(&packet[1], buf, len);
         bt_nus_send(master_conn, packet, len + 1);
-	}*/
+	}
 }
