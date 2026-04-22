1d1a129d9a2f38c39d3bfe1976fd4853fb8f94cc
HubicekHubik <hubertottcz@gmail.com>
2026-04-22 11:15:59 +0200

    addid states handling

--- nRFconnect/Mainprogram/Toy/src/main.c
+++ nRFconnect/Mainprogram/Toy/src/main.c
@@ -5,7 +5,7 @@
  */
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(Toy, CONFIG_LOG_DEFAULT_LEVEL);
+LOG_MODULE_REGISTER(Main, CONFIG_LOG_DEFAULT_LEVEL);
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
@@ -40,6 +40,8 @@ LOG_MODULE_REGISTER(Toy, CONFIG_LOG_DEFAULT_LEVEL);
 #include "bat_measures_modul.h"
 /*Modules for BLE comunication*/
 #include "ble_modul.h"
+/*Modules for Game mods*/
+#include "game_modul.h"
 
 #define SYSSOUND_THREAD_PRIORITY 11
 #define SYSSOUND_THREAD_STACK_SIZE 2048
@@ -53,7 +55,7 @@ int main(void)
 {
 	int err;
 	/*nvs storage init*/
-	err = nvs_init_datarec(&volume, &current_category, &device_running);
+	err = nvs_init_datarec(&volume, &current_category, &device_running, &current_game);
 	if (err) {
 		LOG_ERR("NVS initialization failed, err %d", err);
 	}
@@ -92,6 +94,7 @@ int main(void)
 		LOG_ERR("Baterry measure modul initialization failed, err %d", err);
 		return err;
 	}
+
 	k_thread_create(&syssound_thread_data, syssound_stack_area,
 					K_THREAD_STACK_SIZEOF(syssound_stack_area),
 					system_feedback_thread,
@@ -103,17 +106,31 @@ int main(void)
 		LOG_ERR("BLE modul initialization failed, err %d", err);
 		return err;
 	}
+	
+	
+	err = game_modul_init();
+	if (err) {
+		LOG_ERR("GAME modul initialization failed, err %d", err);
+		return err;
+	}
+
 	return 0;
 }
 
 static void system_feedback_thread(void *arg1, void *arg2, void *arg3) {
 	LOG_INF("System feedback thread started");
 	struct toy_events rx_event;
+	struct toy_events last_bat = {0};
+	struct toy_events tx_audio = {0};
+	struct toy_events tx_lra = {.type = MT_LRA, .len = sizeof(struct lra_ev)};
+
     while (true) {
         if(k_msgq_get(&sysfb_msgq, &rx_event, K_FOREVER) == 0) {
+			bool send = false;
+			bool sendaudio = false;
+			bool sendlra = false;
+
 			switch (rx_event.type) {
-				case MT_BT_CONNECTED:
-					k_msgq_put(&aud_event_msgq, &rx_event,K_NO_WAIT);
 				case MT_REQUEST_SD_DATA:
 					struct file_data data_req = {
 						.file_msg_type = MT_REQUEST_SD_DATA
@@ -122,14 +139,17 @@ static void system_feedback_thread(void *arg1, void *arg2, void *arg3) {
 					break;
 				case MT_LSM6DSL_ON:
 				case MT_LSM6DSL_OFF:
+					sendaudio = true;
 					if(!device_running){
 						device_running = true;
 						nvs_save_power_state(device_running);
 						lsm6dsl_ei_wake();
+						tx_audio.type = MT_LSM6DSL_ON;
 						LOG_INF("Toy turned ON");
 					} else {
 						lsm6dsl_sleep();
 						device_running = false;
+						tx_audio.type = MT_LSM6DSL_OFF;
 						LOG_INF("Toy turned OFF");
 						nvs_save_power_state(device_running);
 					}
@@ -144,7 +164,7 @@ static void system_feedback_thread(void *arg1, void *arg2, void *arg3) {
 					}
 					break;
 				case MT_VOL_DOWN:
-					if(volume >= MIN_VOLUME) {
+					if(volume > MIN_VOLUME) {
 						volume -= VOLUME_STEP;
 						nvs_save_volume(volume);
 						LOG_INF("Volume has decreased to %d", volume);
@@ -152,50 +172,103 @@ static void system_feedback_thread(void *arg1, void *arg2, void *arg3) {
 						LOG_WRN("Volume is at minimum %d", volume);
 					}
 					break;
-				case MSG_TYPE_TURNOFF: //maybe should be removed
-					if (device_running) {
-						LOG_INF("Turning off device");
-						device_running = false;
-						lsm6dsl_sleep();
-					} else {
-						LOG_INF("Turning on device");
-						device_running = true;
-						lsm6dsl_ei_wake();
-					}
-					break;
 				case MT_GEST:
-					struct toy_events tx_lmr = {
-						.type = MT_LMR,
-						.payload.lmr.effect = rx_event.payload.gest.type
-					};
-					k_msgq_put(&lmr_msgq, &tx_lmr, K_NO_WAIT);
-					k_msgq_put(&aud_event_msgq, &rx_event, K_NO_WAIT);
+					if (current_game == GAME_MODE_SWAPPED && remote_conn != NULL) {
+						char send_buffer[120];
+						int len = sizeof(rx_event.payload);
+						memcpy(&send_buffer[1], &rx_event.payload, len);
+						bt_nus_send(remote_conn, (const uint8_t *)send_buffer, len + 1);
+						break;
+					}
+					if (current_game == GAME_MODE_CHAINS) {
+						k_msgq_put(&game_ev_msgq, &rx_event, K_NO_WAIT);
+						break;
+					}
+					sendaudio = true;
+					sendlra = true;
+					sendlra = true;
+					tx_lra.payload.lra.effect = rx_event.payload.gest.type;
+					tx_audio.type = MT_GEST;
+					tx_audio.payload.audio.id = rx_event.payload.gest.type;
 					break;
 				case MT_CHANGE_CATEGORY:
+					if(current_game == GAME_MODE_CHAINS) {
+						LOG_INF("current_game is CHAINS to change category change the game mode");
+						break;
+					}
 					current_category= (current_category + 1) % actual_categories_count;
 					nvs_save_categ(current_category);
+					sendaudio = true;
+					tx_audio.type = MT_CHANGE_CATEGORY;
+					tx_audio.payload.audio.id = (uint8_t *)categories[current_category].categ_id;
 					LOG_INF("Current category changed to %d", current_category);
-				break;
-				case MT_LOW_BATERY:
-					struct toy_events tx_low_bat = {
-						.type = MT_GEST,
-						.payload.gest.type = MT_LOW_BATERY,
-					};
-					k_msgq_put(&aud_event_msgq, &tx_low_bat, K_NO_WAIT);
 					break;
+				case MT_LOW_BATERY:
+					sendaudio = true;
+					tx_audio.type = MT_LOW_BATERY;
+					break;
+				case MT_REQ_LASTBAT:
+					memcpy(&rx_event.payload.power, &last_bat.payload.power, sizeof(rx_event.payload.power));
+					last_bat.payload.power.deviceOn = device_running;
+                    send = true; 
+                    break;
+				case MT_CHARGING:
+				case MT_CHARGED:
+					sendlra = true;
+					tx_lra.payload.lra.effect = CHARGING_EFFECT;
 				case MT_BAT_INF:
-					char send_buffer[128];
-					int len = sizeof(rx_event.payload.power);
-					
-					send_buffer[0] = MT_RECV_POW_DATA;
-					memcpy(&send_buffer[1], &rx_event.payload.power, len);
-					//LOG_INF("Send power status data to mobile aplication Bat_mV: %d, Charging: %d, Charged: %d", rx_event.payload.power.V_bat, rx_event.payload.power.charging, rx_event.payload.power.charged);
-					bt_nus_send(master_conn, (const uint8_t *)send_buffer, len + 1);
+					memcpy(&last_bat.payload.power, &rx_event.payload.power, sizeof(rx_event.payload.power));
+					send = true;
+					break;
+				case MT_G_MODE_CHANGE:
+					current_game= (current_game + 1) % GAME_MODE_CNT;
+					nvs_save_gameM(current_game);
+					LOG_INF("Current game mode changed to %d", current_game);
+					sendaudio = true;
+					tx_audio.type = MT_G_MODE_CHANGE;
+					tx_audio.payload.gest.type = current_game;
+					if(current_game == GAME_MODE_CHAINS) {
+						current_category = ctg_piano_idx;
+						nvs_save_categ(current_category);
+					} 
+					break;
+				case MT_SWITCH_TOY:
+					sendlra = true;
+					tx_lra.payload.lra.effect = CONNECTED_EFFECT;
+					break;
+				case MT_BT_CONNECTED:
+					sendaudio = true;
+					tx_audio.type = MT_BT_CONNECTED;
+					break;
+				case MT_BT_DISCONNECT:
+					sendaudio = true;
+					tx_audio.type = MT_BT_DISCONNECT;
 					break;
 				default:
 					LOG_WRN("Unknown system feedback message received: %d", rx_event.type);
 					break;
 			}
+			if (sendlra) {
+				k_msgq_put(&lra_msgq, &tx_lra, K_NO_WAIT);
+			}
+			
+			if(sendaudio) {
+				k_msgq_put(&aud_event_msgq, &tx_audio, K_NO_WAIT);
+			}
+			if(send && master_conn != NULL) {
+                char send_buffer[128];
+				int len = sizeof(rx_event.payload.power);
+                send_buffer[0] = MT_RECV_POW_DATA;
+                memcpy(&send_buffer[1], &rx_event.payload.power, len);
+
+                bt_nus_send(master_conn, (const uint8_t *)send_buffer, len + 1);
+                
+                LOG_INF("BT Send -> V_bat: %d, Charging: %d, Charged: %d, DeviceON: %d",
+                        rx_event.payload.power.V_bat,
+                        rx_event.payload.power.charging,
+						rx_event.payload.power.charged,
+                        rx_event.payload.power.deviceOn);
+            }
 		}
     }
 }
