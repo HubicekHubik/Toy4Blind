e1b25f9d95a892837950376156dd9e0b6b299def
HubicekHubik <hubertottcz@gmail.com>
2026-04-22 11:09:44 +0200

    revorked rx audio event handling

--- nRFconnect/Mainprogram/Toy/src/audio_modul.c
+++ nRFconnect/Mainprogram/Toy/src/audio_modul.c
@@ -13,7 +13,7 @@
 
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(audio, CONFIG_LOG_DEFAULT_LEVEL);
+LOG_MODULE_REGISTER(audio_modul, CONFIG_LOG_DEFAULT_LEVEL);
 
 #include <zephyr/drivers/i2s.h>
 
@@ -44,34 +44,60 @@ static STRUCT_SECTION_ITERABLE(k_mem_slab, tx_0_mem_slab) =
 						   WB_UP(BLOCK_SIZE), SLAB_COUNT);
 
 #ifdef CONFIG_I2S
-#define AUDIO_HAPTIC_THREAD_STACK_SIZE 2048
-#define AUDIO_HAPTIC_THREAD_PRIORITY 3
-K_THREAD_STACK_DEFINE(audio_haptic_stack_area, AUDIO_HAPTIC_THREAD_STACK_SIZE);
+#define AUDIO_THREAD_STACK_SIZE 2048
+#define AUDIO_THREAD_PRIORITY 3
+K_THREAD_STACK_DEFINE(audio_haptic_stack_area, AUDIO_THREAD_STACK_SIZE);
 static struct k_thread audio_haptic_thread_data;
 
-void audio_haptic_feedback_thread(void *arg1, void *arg2, void *arg3);
+typedef enum {
+    SYS_SOUND_LOW_BATTERY,
+    SYS_SOUND_BT_CONNECTED,
+    SYS_SOUND_BT_DISCONNECTED,
+    SYS_SOUND_MODE_DEFAULT,
+    SYS_SOUND_MODE_CHAINS,
+    SYS_SOUND_ZAPNUTO,
+    SYS_SOUND_VYPNUTO,
+	SYS_SOUND_MODE_MIRROR,
+	SYS_SOUND_MODE_SWAPPED,
+	SYS_SOUND_CATEG_CHANGED,
+
+	SYS_SOUND_COUNT
+} sys_sound_t;
+
+static const char *const sys_sound_paths[] = {
+    [SYS_SOUND_LOW_BATTERY]    = "SD:/system/lowB.wav",
+    [SYS_SOUND_BT_CONNECTED]   = "SD:/system/btOn.wav",
+    [SYS_SOUND_BT_DISCONNECTED]= "SD:/system/btOff.wav",
+    [SYS_SOUND_MODE_DEFAULT]     = "SD:/system/Bezny.wav",
+    [SYS_SOUND_MODE_CHAINS]  = "SD:/system/Retezovy.wav",
+	[SYS_SOUND_MODE_MIRROR]	   = "SD:/system/Zrcadlovy.wav",
+	[SYS_SOUND_MODE_SWAPPED]   = "SD:/system/Obraceny.wav",
+	[SYS_SOUND_VYPNUTO]        = "SD:/system/Vypnout.wav",
+	[SYS_SOUND_ZAPNUTO]		   = "SD:/system/Zapnout.wav",
+	[SYS_SOUND_CATEG_CHANGED]  = "SD:/system/ctg_chng/"
+};
+
+void audio_feedback_thread(void *arg1, void *arg2, void *arg3);
 #endif
-
-
-
-void LMR_control_thread(void *arg1, void *arg2, void *arg3);
 
 int audio_modul_init() {
 
 	#ifdef CONFIG_I2S
     k_thread_create(&audio_haptic_thread_data, audio_haptic_stack_area,
                 K_THREAD_STACK_SIZEOF(audio_haptic_stack_area),
-                audio_haptic_feedback_thread,
+                audio_feedback_thread,
                 NULL, NULL, NULL,
-                AUDIO_HAPTIC_THREAD_PRIORITY, 0, K_NO_WAIT);
+                AUDIO_THREAD_PRIORITY, 0, K_NO_WAIT);
 	#endif
     return 0;
 }
 
 #ifdef CONFIG_I2S
-int playaudio(const struct device *dev_i2s, const char *path);
+int find_piano();
 
-int i2s_initialize(const struct device *dev_i2s); // init of i2s for playing audio
+int playaudio(const struct device *dev_i2s, const char **paths, uint8_t files_num);
+
+int i2s_initialize(const struct device *dev_i2s); // init of i2s for playing gest
 
 int i2s_initialize(const struct device *dev_i2s){
 
@@ -92,7 +118,7 @@ int i2s_initialize(const struct device *dev_i2s){
 	i2s_cfg.format = I2S_FMT_DATA_FORMAT_I2S;
 	i2s_cfg.frame_clk_freq = 44100;
 	i2s_cfg.block_size = BLOCK_SIZE;
-	i2s_cfg.timeout = 1000;
+	i2s_cfg.timeout = 3000;
 	i2s_cfg.options = I2S_OPT_FRAME_CLK_MASTER | I2S_OPT_BIT_CLK_MASTER;
 	i2s_cfg.mem_slab = &tx_0_mem_slab;
 
@@ -106,10 +132,9 @@ int i2s_initialize(const struct device *dev_i2s){
 	return 0;
 }
 
-void audio_haptic_feedback_thread(void *arg1, void *arg2, void *arg3) {
+void audio_feedback_thread(void *arg1, void *arg2, void *arg3) {
 	int ret;
 	struct toy_events rx_ah;
-
 	LOG_INF("Audio/haptic thread starting...");
 
 	const struct device *dev_i2s = DEVICE_DT_GET(DT_ALIAS(i2stx));
@@ -124,33 +149,102 @@ void audio_haptic_feedback_thread(void *arg1, void *arg2, void *arg3) {
 	while (true) {
 		if (k_msgq_get(&aud_event_msgq, &rx_ah, K_FOREVER) == 0) {
 			char temp_path[MAX_PATH];
+			char temp_path2[MAX_PATH];
+			char temp_path3[MAX_PATH];
+			const char *paths[CHAIN_TRESHOLD] = {NULL, NULL, NULL};
+			int num_to_play = 1;
 
 			const char *result_path = NULL;
-			
+			LOG_INF("Recieved packet type 0x%2x gest type %d", rx_ah.type, rx_ah.payload.gest.type);
 			switch (rx_ah.type) {
 				case MT_GEST:
-					result_path = draw_audio_path(rx_ah.payload.gest.type);
+					result_path = draw_audio_path(rx_ah.payload.gest.type, 255);
 					if ( result_path != NULL) {
-						strncpy(temp_path, result_path, sizeof(temp_path) - 1);
+						strncpy(temp_path, result_path, MAX_PATH - 1);
 					}
+					paths[0] = temp_path;
+					break;
+				case MT_CHAIN:
+					LOG_INF("Recieved chain event");
+					/*
+					num_to_play = 3;
+					if (ctg_piano_idx != -1) {
+						LOG_INF("Piano found at category index: %d", ctg_piano_idx);
+					} else {
+						LOG_WRN("Piano directory (P_...) not found on SD card!");
+						break;
+					}
+					
+					for (size_t i = 0; i < CHAIN_TRESHOLD; i++) {
+						result_path = draw_audio_path(rx_ah.payload.audio.chain[i], ctg_piano_idx);
+						if ( result_path != NULL && i == 0) {
+							strncpy(temp_path, result_path, MAX_PATH - 1);
+							paths[0] = temp_path;
+						}
+						if ( result_path != NULL && i == 1) {
+							strncpy(temp_path2, result_path, MAX_PATH - 1);
+							paths[1] = temp_path2;
+						}
+						if ( result_path != NULL && i == 2) {
+							strncpy(temp_path3, result_path, MAX_PATH - 1);
+							paths[2] = temp_path3;
+						}
+					}
+					*/
+
+					result_path = permute_and_find(rx_ah.payload.audio.chain);
+					if ( result_path != NULL) {
+						strncpy(temp_path, result_path, MAX_PATH - 1);
+					}
+					paths[0] = temp_path;
 					break;
 				case MT_LOW_BATERY:
-					strncpy(temp_path, "SD:/system/lowB.wav", sizeof(temp_path) - 1);
+					paths[0] = sys_sound_paths[SYS_SOUND_LOW_BATTERY];
 					break;
 				case MT_BT_CONNECTED:
-					strncpy(temp_path, "SD:/system/btOn.wav", sizeof(temp_path) - 1);
+					paths[0] = sys_sound_paths[SYS_SOUND_BT_CONNECTED];
 					break;
 				case MT_BT_DISCONNECT:
-					strncpy(temp_path, "SD:/system/btOff.wav", sizeof(temp_path) - 1);
+					paths[0] = sys_sound_paths[SYS_SOUND_BT_DISCONNECTED];
+					break;
+				case MT_LSM6DSL_OFF:
+					paths[0] = sys_sound_paths[SYS_SOUND_VYPNUTO];
+					break;
+				case MT_LSM6DSL_ON:
+					paths[0] = sys_sound_paths[SYS_SOUND_ZAPNUTO];
+					break;
+				case MT_G_MODE_CHANGE:
+					switch (rx_ah.payload.gest.type) {
+						case GAME_MODE_DEFAULT:
+							paths[0] = sys_sound_paths[SYS_SOUND_MODE_DEFAULT];
+							break;
+						case GAME_MODE_CHAINS:
+							paths[0] = sys_sound_paths[SYS_SOUND_MODE_CHAINS];
+							break;
+						case GAME_MODE_MIRROR:
+							paths[0] = sys_sound_paths[SYS_SOUND_MODE_MIRROR];
+							break;
+						case GAME_MODE_SWAPPED:
+							paths[0] = sys_sound_paths[SYS_SOUND_MODE_SWAPPED];
+							break;
+						default:
+							LOG_INF("Unknown gamemode in gest haptic thread: 0x%2x", rx_ah.type);
+							break;
+					}
+					break;
+				case MT_CHANGE_CATEGORY:
+						sprintf(temp_path, "%s%c.wav",sys_sound_paths[SYS_SOUND_CATEG_CHANGED], rx_ah.payload.gest.type);
+						LOG_INF("Categ change sys path is %s",temp_path);
+						paths[0] = temp_path;
 					break;
 				default:
-					LOG_INF("Unknown comand in audio haptic thread: %d", rx_ah.type);
+					LOG_INF("Unknown comand in gest haptic thread: %d", rx_ah.type);
 					break;
 			}
-			if(temp_path != NULL) {
-				playaudio(dev_i2s, temp_path);
+			if(paths[0][0] != '\0') {
+				playaudio(dev_i2s, paths, num_to_play);
 			}else{
-				LOG_INF("Someting went wrong while looking for path skipping audio paly");
+				LOG_INF("Someting went wrong while looking for path skipping gest paly");
 			}
 		}
 	}
@@ -158,29 +252,31 @@ void audio_haptic_feedback_thread(void *arg1, void *arg2, void *arg3) {
 #endif
 
 #if defined(CONFIG_I2S) && defined(CONFIG_FILE_SYSTEM)
-int playaudio(const struct device *dev_i2s, const char *path) {
+int playaudio(const struct device *dev_i2s, const char **paths, uint8_t files_num) {
 	k_sem_take(&sd_data_sem, K_FOREVER);
 	audioPlaying = true;
 	void *audio_block;
-	int ret;
+	int ret = 0;
 
-	static uint8_t audio_buf[AUDIO_SD_CHUNK];
+	static uint8_t audio_buf[CHAIN_TRESHOLD][AUDIO_SD_CHUNK];
 
-	UINT bytes_read;
+	UINT bytes_read[CHAIN_TRESHOLD];
 	FRESULT fr;
 
-	FIL wav;
-	fr = f_open(&wav, path, FA_READ);
-	if (fr) {
-		LOG_ERR("open %s failed (%d)", path, fr);
-		return -EIO;
-	} else {
-		LOG_INF("Playing audio from path: %s and skipping header",path);
-		f_lseek(&wav, 44); // skip WAV header
-	}
+	FIL wav[CHAIN_TRESHOLD];
 
+	for (size_t i = 0; i < files_num; i++) {
+		if (f_open(&wav[i], paths[i], FA_READ)) {
+			LOG_ERR("open %s failed (%d)", paths[i], fr);
+			ret = -EIO;
+			goto cleanup;
+		} else {
+		LOG_INF("Playing from path: %s and skipping header",paths[i]);
+		f_lseek(&wav[i], 44); // skip WAV header
+		}
+	}
 	// i2s warm up
-	for (int j = 0; j < 1; j++) {
+	for (int j = 0; j < 3; j++) {
 		ret = k_mem_slab_alloc(&tx_0_mem_slab, &audio_block, K_FOREVER);
 		if (ret != 0)
 		{
@@ -202,34 +298,50 @@ int playaudio(const struct device *dev_i2s, const char *path) {
 		LOG_ERR("Could not start I2S TX error: %d", ret);
 		return ret;
 	}
-
-	while ((fr = f_read(&wav, audio_buf, AUDIO_SD_CHUNK, &bytes_read)) == FR_OK && bytes_read > 0)
-	{
-		int16_t *samples = (int16_t *)audio_buf;
-		size_t count = bytes_read / 2; // 16bit PCM
-
-		for (size_t i = 0; i < count; i++) {
-			float scaled = samples[i] * (volume / 10.0f);
-
-			// přetečení – clamp
-			if (scaled > 32767.0f)
-				scaled = 32767.0f;
-			if (scaled < -32768.0f)
-				scaled = -32768.0f;
-
-			samples[i] = (int16_t)scaled;
+	while (true) {
+		bool any_data = false;
+		
+		for (size_t i = 0; i < files_num; i++) {
+			if ((fr = f_read(&wav[i], audio_buf[i], AUDIO_SD_CHUNK, &bytes_read[i])) == FR_OK) {
+				if(bytes_read[i] > 0) {
+					any_data = true;
+					//LOG_INF("FILE %d and bytes read is %d", i, bytes_read[i]);
+				}
+			}
 		}
 
-		// Alokace bloku
+		if (!any_data) break;
+
+		// I2S block allocation
 		ret = k_mem_slab_alloc(&tx_0_mem_slab, &audio_block, K_FOREVER);
 		if (ret != 0) {
 			LOG_ERR("TX block allocation failed %d", ret);
 			continue;
 		}
+		
+		int16_t *out_samples = (int16_t *)audio_block;
+		size_t sample_count = AUDIO_SD_CHUNK / 2;
 
-		memcpy(audio_block, audio_buf, bytes_read);
+		for (size_t i = 0; i < sample_count; i++) {
 
-		// Odeslat blok
+			int32_t mixed_32 = 0;
+
+			for (size_t s = 0; s < files_num; s++) {
+				if (bytes_read[s] >= (i + 1) * 2) {
+					int16_t *sample_ptr = (int16_t *)&audio_buf[s][i * 2];
+					mixed_32 += *sample_ptr;
+				}
+			}
+			float scaled = (mixed_32 * (int32_t)volume) / 10 * files_num;
+
+			// in case of overflow – clamp
+			if (scaled > 32767) scaled = 32767;
+			if (scaled < -32768) scaled = -32768;
+
+			out_samples[i] = (int16_t)scaled;
+		}
+		
+		// Send block to driver
 		ret = i2s_write(dev_i2s, (uint8_t *)audio_block, BLOCK_SIZE);
 		if (ret != 0) {
 			LOG_ERR("I2S write error: %d", ret);
@@ -241,14 +353,17 @@ int playaudio(const struct device *dev_i2s, const char *path) {
 	i2s_trigger(dev_i2s, I2S_DIR_TX, I2S_TRIGGER_STOP);
 	i2s_trigger(dev_i2s, I2S_DIR_TX, I2S_TRIGGER_DROP);
 
-	fr = f_close(&wav);
-	if (fr != FR_OK) {
-		LOG_ERR("f_close failed: %d", fr);
-		return -EIO;
-	}
+	cleanup:
+		for (int i = 0; i < files_num; i++) {
+			fr = f_close(&wav[i]);
+			if (fr != FR_OK) {
+				LOG_ERR("f_close failed: %d", fr);
+				ret = -EIO;
+			}
+		}
 
-	audioPlaying = false;
-	k_sem_give(&sd_data_sem);
-	return 0;
+		audioPlaying = false;
+		k_sem_give(&sd_data_sem);
+		return ret;
 }
 #endif
--- nRFconnect/Mainprogram/Toy/src/audio_modul.h
+++ nRFconnect/Mainprogram/Toy/src/audio_modul.h
@@ -2,6 +2,7 @@
 #define AUDIO_MODUL_H_
 
 extern struct k_msgq aud_event_msgq;
+
 int audio_modul_init();
 
 #endif
